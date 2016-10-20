67a68399d36d981e78ff7cd80caa3330edcefe0c
Li-Ta Lo <lita.ollie@gmail.com>
2016-10-20 14:09:28 -0600

    move test fixtures to dolfin_triangle_fixture.h

--- flecsi/specializations/dolfin_triangle/test/dolfin_triangle_fixture.h
+++ flecsi/specializations/dolfin_triangle/test/dolfin_triangle_fixture.h
@@ -22,6 +22,22 @@ protected:
   dolfin_triangle_mesh_t<dolfin_triangle_types_t> dolfin;
 };
 
+class A_Dolfin_Triangle_Partitioned_In_One : public ::testing::Test {
+protected:
+  dolfin_triangle_mesh_t<dolfin_triangle_types_t> dolfin;
+
+  virtual void SetUp() {
+    dolfin.compute_graph_partition(0, 2, cell_sizes, cell_partitions);
+  }
+
+  // WARNING: we deliberately use int (which is either 32 or 64-bits
+  // signed integer) for cell_sizes and mesh_graph_partition. This may truncate
+  // the high order bits of FleCSI's id_t (which is essentially a 64-bit
+  // unsigned integer).
+  std::vector<int> cell_sizes = {10};
+  std::vector<topology::mesh_graph_partition<int>> cell_partitions;
+};
+
 class A_Dolfin_Triangle_Partitioned_In_Two : public ::testing::Test {
 protected:
   dolfin_triangle_mesh_t<dolfin_triangle_types_t> dolfin;
